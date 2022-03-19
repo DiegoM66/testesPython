71dae7e91955720bc6c8ba7cabe808217489989a
Diego M <diegomarques66@outlook.com>
2022-03-18 23:00:09 -0300

    first commit

---
+++ teste.py
@@ -0,0 +1,12 @@
+a = 5
+b = 5
+soma = a + b
+soma2 = b * b
+quadrado = soma**2
+
+#print('A  soma é: ', soma, 'Soma  ao quadrado é: ', quadrado)
+if soma == 10:
+   print('soma é 10')
+else:
+   print('Não é 10')
+
---
+++ teste2.py
@@ -0,0 +1,39 @@
+for n in range (5):
+     print(n)
+
+s = "Hello World"
+
+#Primeira letra (H)
+print(s[0])
+
+#Remove 5 primeiros caracteres
+print(s[5:])
+
+#Remove 5 ultimos caracteres
+print(s[:5])
+
+#Inverte
+print(s[::-1])
+
+#Numero de caracteres de s
+print(len(s))
+
+#Multiplicar
+letter = 'z'
+print(letter*10)
+print('\n'*2)
+
+#CapsLock/Caixa Alta
+print(s.upper())
+print('\n')
+
+#Caixa Baixa
+print(s.lower())
+print('\n')
+
+#Dividir palavras
+print(s.split())
+print('\n')
+
+#Dividir elemento especifico
+print(s.split('W'))
